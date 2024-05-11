eb3b8d23d0d39b3ea92c5dd99ef9a9bfe19cd406
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-05-11 12:52:11 +0300

    new idea

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -6,7 +6,7 @@ import css from "./Searchbar.module.css";
 import sprite from "../../assets/search.svg";
 import searchCopied from "../../shared/searchCopied.svg";
 
-import { ReactComponent as MyMagnifire } from "../../shared/searchCopied.svg";
+// import { ReactComponent as MyMagnifire } from "../../shared/searchCopied.svg";
 // import { MyMagnifire } from "../../shared/index";
 
 const Searchbar: React.FC<SearchbarProps> = ({ onSubmit }) => {
@@ -80,7 +80,7 @@ const Searchbar: React.FC<SearchbarProps> = ({ onSubmit }) => {
                   <path d="M31.008 27.231l-7.58-6.447c-0.784-0.705-1.622-1.029-2.299-0.998 1.789-2.096 2.87-4.815 2.87-7.787 0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12c2.972 0 5.691-1.081 7.787-2.87-0.031 0.677 0.293 1.515 0.998 2.299l6.447 7.58c1.104 1.226 2.907 1.33 4.007 0.23s0.997-2.903-0.23-4.007zM12 20c-4.418 0-8-3.582-8-8s3.582-8 8-8 8 3.582 8 8-3.582 8-8 8z"></path>
                 </g>
               </svg>
-              <MyMagnifire />
+              {/* <MyMagnifire /> */}
               {/* experiment */}
             </div>
           </button>
